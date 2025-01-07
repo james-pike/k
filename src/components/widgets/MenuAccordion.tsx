2bc01afd56279fde900d42d53ff7813909c7505d
James <jamesandrewpike@gmail.com>
2025-01-07 13:15:42 -0500

    commit

--- src/components/widgets/MenuAccordion.tsx
+++ src/components/widgets/MenuAccordion.tsx
@@ -9,21 +9,7 @@ export default component$(() => {
         <Accordion.Trigger class="text-xl flex trigger-text justify-between hover:text-[#70C7BA] items-center py-3 border-b border-gray-300 dark:border-gray-700">
           <span>About</span>
         </Accordion.Trigger>
-        <Accordion.Content class=" py-2">
-          {/* Service Links with Highlight and Divider */}
-          <div class="hover:bg-[#70C7BA]/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
-          </div>
-          <div class="hover:bg-[#70C7BA]/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/" class="block text-lg py-3 px-2">Development</a>
-          </div>
-          <div class="hover:bg-[#70C7BA]/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/" class="block text-lg py-3 px-2">SEO Optimization</a>
-          </div>
-          <div class="hover:bg-[#70C7BA]/35 transition">
-            <a href="/" class="block text-lg py-3 px-2">Brand Strategy</a>
-          </div>
-        </Accordion.Content>
+       
       </Accordion.Item>
 
 
@@ -33,21 +19,7 @@ export default component$(() => {
         <Accordion.Trigger class="text-xl flex trigger-text justify-between hover:text-[#70C7BA] items-center py-3 border-b border-gray-300 dark:border-gray-700">
           <span>Roadmap</span>
         </Accordion.Trigger>
-        <Accordion.Content class=" py-2">
-          {/* Service Links with Highlight and Divider */}
-          <div class="hover:bg-[#70C7BA]/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
-          </div>
-          <div class="hover:bg-[#70C7BA]/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/" class="block text-lg py-3 px-2">Development</a>
-          </div>
-          <div class="hover:bg-[#70C7BA]/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/" class="block text-lg py-3 px-2">SEO Optimization</a>
-          </div>
-          <div class="hover:bg-[#70C7BA]/35 transition">
-            <a href="/" class="block text-lg py-3 px-2">Brand Strategy</a>
-          </div>
-        </Accordion.Content>
+       
       </Accordion.Item>
 
 
