064e2310effa72e5e8f4abecfc44a37e183a88d1
James <jamesandrewpike@gmail.com>
2025-01-07 12:31:20 -0500

    commit

--- src/routes/(about)/about/index.tsx
+++ src/routes/(about)/about/index.tsx
@@ -5,13 +5,7 @@ import About from "~/components/widgets/About";
 export default component$(() => {
   return (
     <>
-      <About highlight="About Us"
-        title="Flexible Pricing Options"
-        subtitle="Choose a one-time payment or pay it off over a longer period."
-         items={[ {
-
-          },
-        ]}
+      <About 
       />
       
     </>
