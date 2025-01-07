598083018ec83a84988817d5f8dfd4acdfe29f8a
James <jamesandrewpike@gmail.com>
2025-01-06 22:16:29 -0500

    commit

--- src/components/widgets/Roadmap.tsx
+++ src/components/widgets/Roadmap.tsx
@@ -1,28 +1,17 @@
 import { component$ } from '@builder.io/qwik';
-import { Accordion } from '../ui/Accordion';
+import { Image } from "@unpic/qwik";
+
 
 export default component$(() => {
   return (
-    <Accordion.Root class="w-full dark:text-gray-50">
-      <Accordion.Item>
-        <Accordion.Trigger header="h2">Is it accessible?</Accordion.Trigger>
-        <Accordion.Content>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </Accordion.Content>
-      </Accordion.Item>
-      <Accordion.Item>
-        <Accordion.Trigger>Is it styled?</Accordion.Trigger>
-        <Accordion.Content>
-          Yes. It comes with default styles that matches the other components&apos;
-          aesthetic.
-        </Accordion.Content>
-      </Accordion.Item>
-      <Accordion.Item>
-        <Accordion.Trigger>Is it animated?</Accordion.Trigger>
-        <Accordion.Content>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </Accordion.Content>
-      </Accordion.Item>
-    </Accordion.Root>
+    <Image
+    src="/images/koin.png"
+    layout="constrained"
+    width={1920}
+    height={1080}
+    alt="Full-Width Hero Image"
+    class="w-full h-[90vh] object-cover"
+    priority={true}
+  />
   );
 });
