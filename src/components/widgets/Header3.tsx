25881c8279d7ca1dff01d3a6c23355e64b3c3e52
James <jamesandrewpike@gmail.com>
2025-01-07 13:14:42 -0500

    commit

--- src/components/widgets/Header3.tsx
+++ src/components/widgets/Header3.tsx
@@ -74,7 +74,7 @@ export default component$(() => {
                       </Link>
       
                       <Link
-                        class="text-gray-500 dark:text-gray-400 mr-1 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center"
+                        class="text-gray-500 dark:text-gray-400 mr-2 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center"
                         aria-label={"X"}
                         title={"X"}
                         href={"https://x.com/kaskrc20token"}
